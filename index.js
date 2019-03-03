71d1a66a876c44dc941cdd3b0ad531eff7fd94c2
Hasdrubal <ronaldolsthoorn1@gmail.com>
2019-03-03 18:36:10 +0100

    first working version

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import Twitter from 'twitter';
 import React, { Component } from 'react';
 import config from './config.json';
 
@@ -7,11 +6,11 @@ export default class TwitterFetcher extends Component {
 
   constructor(props) {
     super(props);
-    this.tag = config.config.tag;
     this.interval = config.config.interval;
-    this.consumer_key = config.config.consumer_key;
-    this.consumer_secret = config.config.consumer_secret;
-    this.bearer_token = config.config.bearer_token;
+    this.url = config.config.url;
+        this.state = {
+        tweets: []
+    };
   }
 
   componentDidMount() {
@@ -32,44 +31,50 @@ export default class TwitterFetcher extends Component {
   }
 
   updateFeed(){
+    fetch(this.url)
+      .then(res =>{
+        res.json().then(
+          result =>{
+            this.setState({
+              tweets: result.tweets
+            });
+          }
+        );
+      }
+    );
 
-    console.log(this.client)
-
-    var client = new Twitter({
-      consumer_key: this.consumer_key,
-      consumer_secret: this.consumer_secret,
-      bearer_token: this.bearer_token
-    });
-
-    client.get('search/tweets', {q: this.tag}, function(error, tweets, response) {
-      if(error) throw error;
-      console.log(tweets);  // The favorites.
-      this.setState({feed: tweets})
-      console.log(response);  // Raw response object.
-   });
-
-    return
+      console.log(this.state)
   }
 
   render() {
 
-    const css = `
-      .my-element {
-        height:100%;
-        display:flex;
-        flex-direction:column;
-        justify-content:center;
+  const css = `
+      .twitter-block {
+        width:30%;
       }
-
-      .my-h1 {
-        text-align: center;
+      .twitter-list{
+        list-style-type:none;
+      }
+      .twitter-item{
+        margin:0 0 1em 0;
       }
     `
 
+  const list = (
+    <ul className="twitter-list">
+      {this.state.tweets.map((tweet) =>
+        <li className={["small", "twitter-item"].join(" ")} key={tweet.id}>
+          {tweet.text}
+        </li>
+      )}
+    </ul>
+  );
+
     return (
-      <div className="my-element">
-        <style>{css}</style>
-        <h1 className="my-h1"></h1>
+      <div className="twitter-block">
+      <style>{css}</style>
+      <p># twitter </p>
+        {list}
       </div>
     );
   }
